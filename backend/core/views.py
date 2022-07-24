4caec744155c171868826ae1b041017a21c1c7d0
Kshitij Raj Sharma <skshitizraj@gmail.com>
2022-07-24 15:26:40 +0545

    changed response and changed api type as well

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,11 +1,13 @@
 from xml.dom import ValidationErr
 from django.shortcuts import render
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers, serializers, viewsets 
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_gis.filters import InBBoxFilter
 from rest_framework import status
 from rest_framework import permissions
+from rest_framework.parsers import JSONParser 
 from django.shortcuts import render, get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import *
@@ -166,61 +168,76 @@ def process_geojson(geojson_file_path,aoi_id):
 
 DEFAULT_TILE_SIZE=254
 DEFAULT_ZOOM_LEVEL=19
+   
+@api_view(['POST'])   
+def image_download_api(request):
+    print(request.data)
+
+    serializer=ImageDownloadSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        dataset_id=request.data.get('dataset_id')
+        try:
+            source =request.data.get('source')
+        except :
+            source = None
+    # need to get all the aoi associated with dataset, that's why filtering except getting 
+    aois = AOI.objects.filter(dataset=dataset_id)
+    # looping through each of them and processing it one by one , later on we can specify each aoi to no of threads available 
+    for obj in aois :
+        # TODO : Here assign each aoi to different thread as much as possible and available 
+        if obj.imagery_status != 0 :
+            print(f"Running Download process for aoi : {obj.id} - dataset : {dataset_id}")
+            obj.imagery_status = 0
+            obj.save()
+            # print(obj.geom.coords[0])
+            bbox_coords=bbox(obj.geom.coords[0])
+            print(f"bbox is : {bbox_coords}")
     
-class DatasetApiView(APIView):
-    def post(self, request,dataset_id,source=None, *args, **kwargs):
-        # need to get all the aoi associated with dataset, that's why filtering except getting 
-        aois = AOI.objects.filter(dataset=dataset_id)
-        # looping through each of them and processing it one by one , later on we can specify each aoi to no of threads available 
-        for obj in aois :
-            # TODO : Here assign each aoi to different thread as much as possible and available 
-            if obj.imagery_status != 0 :
-                print(f"Running Download process for aoi : {obj.id} - dataset : {dataset_id}")
-                obj.imagery_status = 0
-                obj.save()
-                # print(obj.geom.coords[0])
-                bbox_coords=bbox(obj.geom.coords[0])
-                print(f"bbox is : {bbox_coords}")
-        
-                tile_size = DEFAULT_TILE_SIZE # by default 
-                zm_level=DEFAULT_ZOOM_LEVEL
-                
-                # start point where we will start downloading the tiles
-    
-                start_point_lng=bbox_coords[0] #getting the starting lat lng
-                start_point_lat=bbox_coords[1]
-                
-                # end point where we should stop downloading the tile 
-                end_point_lng=bbox_coords[2] #getting the ending lat lng
-                end_point_lat=bbox_coords[3]
-                
-                # Note :  lat=y-axis, lng=x-axis 
-                # getting tile coordinate for first point of bbox
-                start_x,start_y=latlng2tile(zoom=zm_level,lat=start_point_lat,lng=start_point_lng,tile_size=tile_size)
-                start=[start_x,start_y]
-                
-                # getting tile coordinate for last point of bbox
-                end_x,end_y=latlng2tile(zoom=zm_level,lat=end_point_lat,lng=end_point_lng,tile_size=tile_size)
-                end=[end_x,end_y]
-                try:
-                    # start downloading 
-                    if source : # if source is supplied from post request itself
-                        download_imagery(start,end,zm_level,dataset_id=dataset_id,source=source)
-                    else:
-                        download_imagery(start,end,zm_level,dataset_id=dataset_id) 
-                    obj.imagery_status = 1
-                    obj.last_fetched_date = datetime.datetime.utcnow()
-                    obj.save()
-                    
-                except Exception as ex : # if download process is failed somehow then it should be indicated as not downloaded
-                    print(ex)
-                    obj.imagery_status = -1
-                    obj.last_fetched_date = datetime.datetime.utcnow()
-                    obj.save()
-            else:
-                print(f"There is running process already for : {obj.id} - dataset : {dataset_id} , Skippinggg")
+            tile_size = DEFAULT_TILE_SIZE # by default 
+            zm_level=DEFAULT_ZOOM_LEVEL
             
-        return Response("Download Success", status=status.HTTP_201_CREATED)
+            # start point where we will start downloading the tiles
+
+            start_point_lng=bbox_coords[0] #getting the starting lat lng
+            start_point_lat=bbox_coords[1]
+            
+            # end point where we should stop downloading the tile 
+            end_point_lng=bbox_coords[2] #getting the ending lat lng
+            end_point_lat=bbox_coords[3]
+            
+            # Note :  lat=y-axis, lng=x-axis 
+            # getting tile coordinate for first point of bbox
+            start_x,start_y=latlng2tile(zoom=zm_level,lat=start_point_lat,lng=start_point_lng,tile_size=tile_size)
+            start=[start_x,start_y]
+            
+            # getting tile coordinate for last point of bbox
+            end_x,end_y=latlng2tile(zoom=zm_level,lat=end_point_lat,lng=end_point_lng,tile_size=tile_size)
+            end=[end_x,end_y]
+            try:
+                # start downloading 
+                if source : # if source is supplied from post request itself
+                    download_imagery(start,end,zm_level,dataset_id=dataset_id,source=source)
+                else:
+                    download_imagery(start,end,zm_level,dataset_id=dataset_id) 
+                obj.imagery_status = 1
+                obj.last_fetched_date = datetime.datetime.utcnow()
+                obj.save()
+                
+            except Exception as ex : # if download process is failed somehow then it should be indicated as not downloaded
+                print(ex)
+                obj.imagery_status = -1
+                obj.last_fetched_date = datetime.datetime.utcnow()
+                obj.save()
+        else:
+            print(f"There is running process already for : {obj.id} - dataset : {dataset_id} , Skippinggg")
+    aoi = AOI.objects.filter(dataset=dataset_id).values()
+    
+    res_serializer=ImageDownloadResponseSerializer(data=list(aoi),many=True) 
+    
+    
+    if res_serializer.is_valid(raise_exception=True):
+        print(res_serializer.data)
+        return Response(res_serializer.data, status=status.HTTP_201_CREATED)
             
 def bbox(coord_list):
     """_summary_
@@ -283,9 +300,11 @@ def download_imagery(start : list , end :list , zm_level , dataset_id , source='
     
     start_x=begin_x # starting loop from beginning 
     start_y=begin_y # starting y loop from beginnig
+    source_name="OAM" #default
     
     while start_x <= stop_x: # download for x section while keeping y as constant 
         download_path= [start_x,begin_y]
+        
         
         if source == 'maxar': 
             try : 
@@ -294,13 +313,14 @@ def download_imagery(start : list , end :list , zm_level , dataset_id , source='
                 raise ImportError(
                         "Connect id for maxar is not supplied"
                 )
+            source_name=source
             download_url=f"https://services.digitalglobe.com/earthservice/tmsaccess/tms/1.0.0/DigitalGlobe:ImageryTileService@EPSG:3857@jpg/{zm_level}/{download_path[0]}/{download_path[1]}.jpg?connectId={connect_id}&flipy=true"
         
         #add multiple logic on supported sources here 
         else:
             # source should be url as string , like this :  https://tiles.openaerialmap.org/62dbd947d8499800053796ec/0/62dbd947d8499800053796ed/{z}/{x}/{y}
             download_url=source.format(x=download_path[0],y=download_path[1],z=zm_level)
-        with open(f"{base_path}/{source}-{start_x}-{begin_y}-{zm_level}.png", 'wb') as handle:
+        with open(f"{base_path}/{source_name}-{start_x}-{begin_y}-{zm_level}.png", 'wb') as handle:
             response = requests.get(download_url, stream=True)
 
             if not response.ok:
