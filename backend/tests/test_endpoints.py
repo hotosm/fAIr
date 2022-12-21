64556e9be2f84c38c776982219160442b820ac82
kshitijrajsharma <skshitizraj@gmail.com>
2022-12-21 12:26:09 +0545

    mockup

--- backend/tests/test_endpoints.py
+++ backend/tests/test_endpoints.py
@@ -1,3 +1,8 @@
 from rest_framework.test import APIRequestFactory
 
 factory = APIRequestFactory()
+
+
+def test_dataset():
+    response = factory.get("api/v1/dataset/")
+    assert response.status_code == 200
