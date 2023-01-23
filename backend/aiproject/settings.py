2af56132a5a4b8d386ae886e775053ee3560b22e
kshitijrajsharma <skshitizraj@gmail.com>
2023-01-23 20:51:02 +0545

    added support for env cors

--- backend/aiproject/settings.py
+++ backend/aiproject/settings.py
@@ -85,13 +85,11 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-ALLOWED_ORIGINS = env("CORS_ALLOWED_ORIGINS", default="http://127.0.0.1:8000")
+ALLOWED_ORIGINS = env("CORS_ALLOWED_ORIGINS", default="http://127.0.0.1:8000").split(
+    ","
+)
 
-CORS_ORIGIN_WHITELIST = [
-    ALLOWED_ORIGINS,
-    "https://www.openstreetmap.org",
-    "https://mapwith.ai",
-]
+CORS_ORIGIN_WHITELIST = ALLOWED_ORIGINS
 
 # CORS_ORIGIN_ALLOW_ALL = True
 
