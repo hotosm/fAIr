3fb60ca2dbc7e3d88e554c3c60de418efcf97ca7
kshitijrajsharma <skshitizraj@gmail.com>
2023-01-23 20:41:26 +0545

    test with env for cors

--- backend/aiproject/settings.py
+++ backend/aiproject/settings.py
@@ -86,15 +86,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 ALLOWED_ORIGINS = env("CORS_ALLOWED_ORIGINS", default="http://127.0.0.1:8000")
-
-CORS_ORIGIN_WHITELIST = [
-    ALLOWED_ORIGINS,
-    "https://www.openstreetmap.org",
-    "https://mapwith.ai",
-]
-# CORS_ALLOWED_ORIGINS = ["*"]
-
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = [ALLOWED_ORIGINS]
+# CORS_ORIGIN_WHITELIST = [
+#     ALLOWED_ORIGINS,
+#     "https://www.openstreetmap.org",
+#     "https://mapwith.ai",
+# ]
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
