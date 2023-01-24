17649bfdf705f0fb9d13310740bc256c9fb44749
Omran NAJJAR <omran.najjar@gmail.com>
2023-01-24 14:37:09 +0100

    formating

--- frontend/src/components/Layout/TrainingDS/DatasetEditor/DatasetEditor.js
+++ frontend/src/components/Layout/TrainingDS/DatasetEditor/DatasetEditor.js
@@ -6,9 +6,9 @@ import { useEffect, useState } from "react";
 import TileServerList from "./TileServerList";
 import TMProject from "./TMProject";
 import MapActions from "./MapActions";
-import axios from '../../../../axios'
+import axios from "../../../../axios";
 import { useMutation } from "react-query";
-import {  useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 function DatasetEditor() {
   const [mapLayers, setMapLayers] = useState([]);
@@ -30,74 +30,73 @@ function DatasetEditor() {
   const addImageryHandler = (imageryDetails, url) => {
     setOAMImagery({ ...imageryDetails, url });
   };
-   const AddtoMapHandler = (geoJSON) => {
-      setgeoJSON(geoJSON);
+  const AddtoMapHandler = (geoJSON) => {
+    setgeoJSON(geoJSON);
   };
   const getDataset = async (id) => {
     try {
-     
-    
       const res = await axios.get("/dataset/" + id);
 
-      if (res.error) 
-        setError(res.error.response.statusText);
-              
-     console.log("dataset",res.data);
+      if (res.error) setError(res.error.response.statusText);
+
+      console.log("dataset", res.data);
       return res.data;
     } catch (e) {
       console.log("isError");
       setError(e);
     } finally {
-      
     }
   };
-  const { mutate, data:dataset, isLoading} = useMutation(getDataset);
+  const { mutate, data: dataset, isLoading } = useMutation(getDataset);
 
   useEffect(() => {
-    mutate(id)
-  
-    return () => {
-      
-    }
-  }, [])
-  
-  return (
-  <>
-    {isLoading && "Loading ............"}
+    mutate(id);
 
-    { dataset &&
-     <Grid container padding={2} spacing={2}>
-      <Grid item xs={6} md={9}>
-        <DatasetMap
-          onMapLayersChange={mapLayersChangedHandler}
-          currentPosision={
-            currentPosision && [currentPosision[0], currentPosision[1]]
-          }
-          zoom={currentPosision && currentPosision[2]}
-          clearCurrentPosision={() => {
-            setCurrentPosision([]);
-          }}
-          oamImagery={oamImagery}
-          geoJSON={geoJSON}
-          emptyPassedgeoJSON={()=>{setgeoJSON(null)}}
-          dataset={dataset}
-        ></DatasetMap>
-       
-      </Grid>
-      <Grid item xs={6} md={3} className="column2"  >
-        <TileServerList
-          navigateToCenter={navigateToCenter}
-          addImagery={addImageryHandler}
-          removeImagery={(e) => {
-            setOAMImagery(null);
-          }}
-          dataset={dataset}
-        ></TileServerList>
-        <AOI oamImagery={oamImagery} mapLayers={mapLayers.filter(i=> i.type ==="aoi")} selectAOIHandler={selectAOIHandler}></AOI>
-        <TMProject addtoMap={AddtoMapHandler}></TMProject>
-        
-      </Grid>
-    </Grid>}
+    return () => {};
+  }, []);
+
+  return (
+    <>
+      {isLoading && "Loading ............"}
+
+      {dataset && (
+        <Grid container padding={2} spacing={2}>
+          <Grid item xs={6} md={9}>
+            <DatasetMap
+              onMapLayersChange={mapLayersChangedHandler}
+              currentPosision={
+                currentPosision && [currentPosision[0], currentPosision[1]]
+              }
+              zoom={currentPosision && currentPosision[2]}
+              clearCurrentPosision={() => {
+                setCurrentPosision([]);
+              }}
+              oamImagery={oamImagery}
+              geoJSON={geoJSON}
+              emptyPassedgeoJSON={() => {
+                setgeoJSON(null);
+              }}
+              dataset={dataset}
+            ></DatasetMap>
+          </Grid>
+          <Grid item xs={6} md={3} className="column2">
+            <TileServerList
+              navigateToCenter={navigateToCenter}
+              addImagery={addImageryHandler}
+              removeImagery={(e) => {
+                setOAMImagery(null);
+              }}
+              dataset={dataset}
+            ></TileServerList>
+            <AOI
+              oamImagery={oamImagery}
+              mapLayers={mapLayers.filter((i) => i.type === "aoi")}
+              selectAOIHandler={selectAOIHandler}
+            ></AOI>
+            <TMProject addtoMap={AddtoMapHandler}></TMProject>
+          </Grid>
+        </Grid>
+      )}
     </>
   );
 }
