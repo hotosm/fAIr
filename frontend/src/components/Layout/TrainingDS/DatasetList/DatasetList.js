821d552b400ea60c2c9525a874c3474582c72fd0
kshitijrajsharma <skshitizraj@gmail.com>
2023-03-28 14:03:41 +0545

    sort default by id desc

--- frontend/src/components/Layout/TrainingDS/DatasetList/DatasetList.js
+++ frontend/src/components/Layout/TrainingDS/DatasetList/DatasetList.js
@@ -1,182 +1,218 @@
-import React, { useState } from 'react'
-import { DataGrid } from '@mui/x-data-grid';
-import { Button, Container, Grid, IconButton, Tooltip, Typography } from '@mui/material';
-import { useQuery } from 'react-query';
-import axios from '../../../../axios'
-import Alert from '@material-ui/lab/Alert';
-import EditIcon from '@material-ui/icons/Edit';
-import AddIcon from '@material-ui/icons/Add';
-import { useNavigate } from 'react-router-dom';
-import ArchiveIcon from '@material-ui/icons/Archive';
-import { trainingDSStatus } from '../../../../utils';
-import OSMUser from '../../../Shared/OSMUser';
+import React, { useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import {
+  Button,
+  Container,
+  Grid,
+  IconButton,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { useQuery } from "react-query";
+import axios from "../../../../axios";
+import Alert from "@material-ui/lab/Alert";
+import EditIcon from "@material-ui/icons/Edit";
+import AddIcon from "@material-ui/icons/Add";
+import { useNavigate } from "react-router-dom";
+import ArchiveIcon from "@material-ui/icons/Archive";
+import { trainingDSStatus } from "../../../../utils";
+import OSMUser from "../../../Shared/OSMUser";
 
-const DEFAULT_FILTER = {"items":[{"columnField":"created_date","id":8537,"operatorValue":"contains"}],"linkOperator":"and","quickFilterValues":[],"quickFilterLogicOperator":"and"}
-const DatasetList = props => {
+const DEFAULT_FILTER = {
+  items: [{ columnField: "created_date", id: 8537, operatorValue: "contains" }],
+  linkOperator: "and",
+  quickFilterValues: [],
+  quickFilterLogicOperator: "and",
+};
+const DatasetList = (props) => {
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const getDatasets = async () => {
+    try {
+      const res = await axios.get("/dataset");
 
-    const [error, setError] = useState(null)
-    const navigate = useNavigate();
-    const getDatasets = async () => {
+      if (res.error) setError(res.error.response.statusText);
+      else {
+        console.log("getDatasets", res.data);
 
-        try {
+        return res.data;
+      }
+    } catch (e) {
+      setError(e);
+    } finally {
+    }
+  };
+  const { data, isLoading, refetch } = useQuery("getDatasets", getDatasets, {
+    refetchInterval: 60000,
+  });
 
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "name",
+      headerName: "Name",
+      width: 250,
+    },
+    {
+      field: "created_at",
+      headerName: "Created at",
 
-            const res = await axios.get("/dataset");
+      width: 200,
+      valueGetter: (params) => {
+        return params.value && new Date(params.value).toLocaleString();
+      },
+    },
+    {
+      field: "last_modified",
+      headerName: "Last modified at",
 
-            if (res.error)
-                setError(res.error.response.statusText);
-            else {
-                console.log("getDatasets", res.data)
-
-                return res.data;
+      width: 200,
+      valueGetter: (params) => {
+        return params.value && new Date(params.value).toLocaleString();
+      },
+    },
+    {
+      field: "source_imagery",
+      headerName: "Imagery",
+      width: 100,
+      renderCell: (params) => {
+        return (
+          <Tooltip
+            title={
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={`${params.value}`}
+              >{`${params.value}`}</a>
             }
-        } catch (e) {
-            setError(e)
+            aria-label="add"
+          >
+            <span> {params.value && "OAM"}</span>
+          </Tooltip>
+        );
+      },
+    },
+    {
+      field: "created_by",
+      headerName: "User",
+      minWidth: 120,
+      renderCell: (params) => {
+        return <OSMUser uid={params.value}></OSMUser>;
+      },
+    },
+    {
+      field: "status",
+      headerName: "Status",
 
-        } finally {
-
-        }
-    };
-    const { data, isLoading ,refetch } = useQuery("getDatasets", getDatasets, { refetchInterval: 60000 });
-
-    const columns = [
-        { field: 'id', headerName: 'ID', width: 90 },
-        {
-            field: 'name',
-            headerName: 'Name',
-            width: 250,
-
-        },
-        {
-            field: 'created_at',
-            headerName: 'Created at',
-
-            width: 200,
-            valueGetter: (params) => {
-
-                return params.value && new Date(params.value).toLocaleString();
-            }
-        },
-        {
-            field: 'last_modified',
-            headerName: 'Last modified at',
-
-            width: 200,
-            valueGetter: (params) => {
-                return params.value && new Date(params.value).toLocaleString();
-            }
-            ,
-        },
-        {
-            field: 'source_imagery',
-            headerName: 'Imagery',
-            width: 100,
-            renderCell: (params) => {
-                return <Tooltip title={<a target='_blank' rel='noreferrer' href={`${params.value}`}>{`${params.value}`}</a>} aria-label="add">
-                    <span> {params.value && "OAM"}</span>
-                </Tooltip>;
-            }
-        },
-        {
-            field: 'created_by',
-            headerName: 'User',
-            minWidth: 120,
-            renderCell: (params) => {
-                return <OSMUser uid={params.value}></OSMUser> 
-                ;
-            }
-        },
-        {
-            field: 'status',
-            headerName: 'Status',
-           
-            renderCell: (params) => {
-                return <>{`${trainingDSStatus(params.value)}`}</>;
-            }
-        },
-        {
-            field: 'actions',
-            headerName: 'Actions',
-            width: 110,
-            renderCell: (params) => {
-                return <><Tooltip title="Edit dataset" aria-label="Edit">
-                <IconButton aria-label="comments"
-                
-                    onClick={(e) => {
-                        navigate(`/training-datasets/${params.row.id}`)
-                    }}>
-                    <EditIcon />
-                </IconButton>
+      renderCell: (params) => {
+        return <>{`${trainingDSStatus(params.value)}`}</>;
+      },
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 110,
+      renderCell: (params) => {
+        return (
+          <>
+            <Tooltip title="Edit dataset" aria-label="Edit">
+              <IconButton
+                aria-label="comments"
+                onClick={(e) => {
+                  navigate(`/training-datasets/${params.row.id}`);
+                }}
+              >
+                <EditIcon />
+              </IconButton>
             </Tooltip>
             <Tooltip title="Archive dataset" aria-label="Archive">
-                <IconButton aria-label="comments"
-                className='margin1'
-                    onClick={(e) => {
-                       console.log('call Archive')
-                    }}>
-                    <ArchiveIcon />
-                </IconButton>
+              <IconButton
+                aria-label="comments"
+                className="margin1"
+                onClick={(e) => {
+                  console.log("call Archive");
+                }}
+              >
+                <ArchiveIcon />
+              </IconButton>
             </Tooltip>
-            </>
-                //  <p>{`${params.row.status} and id ${params.row.id}`}</p>;
-            }
-        }
+          </>
+        );
+        //  <p>{`${params.row.status} and id ${params.row.id}`}</p>;
+      },
+    },
+  ];
 
-    ];
-
-    return <>
-
-        <Grid container padding={2} spacing={2}>
-            <Grid item xs={9}>
-                <Typography variant="h6" component="div">
-                    Training Datasets
-                </Typography>
-            </Grid>
-            <Grid item xs={3}>
-                <Grid container justifyContent="flex-end">
-
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        startIcon={<AddIcon />}
-                        onClick={()=> {navigate("/training-datasets/new")}}
-                    >
-                        Create New
-                    </Button>
-                </Grid>
-
-            </Grid>
-            <Grid item xs={12}>
-                <Typography variant="body1" component="div">
-                    Description long
-                </Typography>
-            </Grid>
-            {error &&
-                <Grid item xs={12}>
-                    <Alert severity="error">{error}</Alert>
-
-                </Grid>}
-            <Grid item xs={12}>
-                {isLoading && <p>Loading ... </p>}
-                {!isLoading && <div style={{ height: 600, width: '100%' }}>
-                    <DataGrid
-                        rows={data}
-                        columns={columns}
-                        pageSize={10}
-                        rowsPerPageOptions={[10]}
-                        disableSelectionOnClick
-                        onFilterModelChange={(filter)=> { console.log('grid filter',filter); localStorage.setItem('dsFilter',JSON.stringify(filter)); refetch();}}
-                        onSortModelChange={(sorter)=> { console.log('grid sorter',sorter); localStorage.setItem('dsSorter', JSON.stringify(sorter)); refetch();}}
-                        filterModel={localStorage.getItem('dsFilter') ? JSON.parse(localStorage.getItem('dsFilter')) : DEFAULT_FILTER}
-                        sortModel={localStorage.getItem('dsSorter')? JSON.parse(localStorage.getItem('dsSorter')) : []}
-                        // TODO: BUG when no filter sorter check
-                    />
-                </div>}
-
-            </Grid>
-
+  return (
+    <>
+      <Grid container padding={2} spacing={2}>
+        <Grid item xs={9}>
+          <Typography variant="h6" component="div">
+            Training Datasets
+          </Typography>
         </Grid>
-    </>;
-}
+        <Grid item xs={3}>
+          <Grid container justifyContent="flex-end">
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<AddIcon />}
+              onClick={() => {
+                navigate("/training-datasets/new");
+              }}
+            >
+              Create New
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1" component="div">
+            Description long
+          </Typography>
+        </Grid>
+        {error && (
+          <Grid item xs={12}>
+            <Alert severity="error">{error}</Alert>
+          </Grid>
+        )}
+        <Grid item xs={12}>
+          {isLoading && <p>Loading ... </p>}
+          {!isLoading && (
+            <div style={{ height: 600, width: "100%" }}>
+              <DataGrid
+                rows={data}
+                columns={columns}
+                pageSize={10}
+                rowsPerPageOptions={[10]}
+                disableSelectionOnClick
+                onFilterModelChange={(filter) => {
+                  console.log("grid filter", filter);
+                  localStorage.setItem("dsFilter", JSON.stringify(filter));
+                  refetch();
+                }}
+                onSortModelChange={(sorter) => {
+                  console.log("grid sorter", sorter);
+                  localStorage.setItem("dsSorter", JSON.stringify(sorter));
+                  refetch();
+                }}
+                filterModel={
+                  localStorage.getItem("dsFilter")
+                    ? JSON.parse(localStorage.getItem("dsFilter"))
+                    : DEFAULT_FILTER
+                }
+                sortModel={
+                  localStorage.getItem("dsSorter")
+                    ? JSON.parse(localStorage.getItem("dsSorter"))
+                    : [{ field: "id", sort: "desc" }]
+                }
+                // TODO: BUG when no filter sorter check
+              />
+            </div>
+          )}
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 export default DatasetList;
