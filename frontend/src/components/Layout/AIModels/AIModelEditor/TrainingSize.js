ccde0fa81a907b9984ff8fdb0d9d2726591d0063
kshitijrajsharma <skshitizraj@gmail.com>
2023-03-29 13:38:06 +0545

    Commented training size console

--- frontend/src/components/Layout/AIModels/AIModelEditor/TrainingSize.js
+++ frontend/src/components/Layout/AIModels/AIModelEditor/TrainingSize.js
@@ -4,14 +4,9 @@ import axios from "../../../../axios";
 
 const TrainingSize = (props) => {
   // const { accessToken,authenticate } = useContext(AuthContext);
-  console.log(props);
 
   const getTrainingSize = async () => {
     // if (!accessToken) return;
-    console.log(
-      `/workspace/dataset_${props.datasetId}/output/training_${props.trainingId}/preprocessed`
-    );
-
     try {
       const res = await axios.get(
         `/workspace/dataset_${props.datasetId}/output/training_${props.trainingId}/preprocessed`
@@ -20,7 +15,7 @@ const TrainingSize = (props) => {
       if (res.error) {
         // setMapError(res.error.response.statusText);
       } else {
-        console.log("TrainingSize ", res.data);
+        // console.log("TrainingSize ", res.data);
         return res.data;
       }
     } catch (e) {
