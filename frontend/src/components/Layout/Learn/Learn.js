c7c9e76ddd6a5d2e069f7259998d821b8404f9c5
Omran NAJJAR <omran.najjar@gmail.com>
2023-08-14 16:37:01 +0200

    fix learn section refresh error

--- frontend/src/components/Layout/Learn/Learn.js
+++ frontend/src/components/Layout/Learn/Learn.js
@@ -23,105 +23,105 @@ const Learn = () => {
       title: "Step 1: First Login & Click on the button Start Creating Dataset",
       description:
         "To create a new training dataset, start by clicking on the 'Start Creating Dataset' button.",
-      image: "/learn/1.png",
+      image: "/learn-resources/1.png",
     },
     {
       id: 2,
       title: "Step 2: Click on the button Create New",
       description:
         "After clicking on 'Start Creating Dataset', click on the 'Create New' button to create a new dataset.",
-      image: "/learn/2.png",
+      image: "/learn-resources/2.png",
     },
     {
       id: 3,
       title: "Step 3: Click on the input field , Give your Dataset Name",
       description:
         "To name your dataset, click on the input field and type in your desired name & click on the 'Create Training Dataset'",
-      image: "/learn/3.png",
+      image: "/learn-resources/3.png",
     },
     {
       id: 4,
       title: "Step 4: Find Image to Train",
       description:
         "After creating Dataset You will see following screen , Now Open Open Aerial Map in New Tab , https://openaerialmap.org/",
-      image: "/learn/4.png",
+      image: "/learn-resources/4.png",
     },
     {
       id: 5,
       title: "Step 5: Find Drone Image and Copy TMS URL",
       description:
         "Look for your area and find good Drone Image for training . After finding , Click on Copy Image URL TMS",
-      image: "/learn/5.png",
+      image: "/learn-resources/5.png",
     },
     {
       id: 6,
       title: "Step 6: Paste TMS To Open Aerial Imagery Tab",
       description:
         "Paste your TMS URL that you have copied from Open Aerial Map Drone Image",
-      image: "/learn/6.png",
+      image: "/learn-resources/6.png",
     },
     {
       id: 7,
       title: "Step 7: Zoom to Layer and Visualize Image",
       description:
         "Click on Zoom to layer next to your Image name in OAM Block Top Right side of screen",
-      image: "/learn/7.png",
+      image: "/learn-resources/7.png",
     },
     {
       id: 8,
       title: "Step 8: Create Area of Interest for Training",
       description:
         "Click on top left map buttons below zoom , To create AOI , AOI will be used to create labels. Labels are Buildings that will be acting as input for model inside AOI",
-      image: "/learn/8.png",
+      image: "/learn-resources/8.png",
     },
     {
       id: 9,
       title: "Step 9: Fetch Exisiting OSM Buildings in your Area of Interest",
       description:
         "Click on Fetch OSM Data button next to OSM Logo Inside List of Area of Interest on Right side.",
-      image: "/learn/9.png",
+      image: "/learn-resources/9.png",
     },
     {
       id: 10,
       title: "Step 10: Visualize each Buildings to check their accuracy",
       description:
         "Zoom to Level 20 in Map to see OSM Buildings that you have just fetched",
-      image: "/learn/10.png",
+      image: "/learn-resources/10.png",
     },
     {
       id: 11,
       title: "Step 11: Correct your labels",
       description:
         "For training it is crucial that buildings are aligned exactly on Drone image, More good your input is more good your output will be. If you want to edit it Click on OSM Logo and Fix buildings / labels in your AOI",
-      image: "/learn/11.png",
+      image: "/learn-resources/11.png",
     },
     {
       id: 12,
       title: "Step 12: Upload your fixes",
       description:
         "You should see your AOI in ID , Fix your labels and Upload your changes to OSM",
-      image: "/learn/osm.png",
+      image: "/learn-resources/osm.png",
     },
     {
       id: 13,
       title: "Step 13: Come Back to fAIr , Fetch OSM Labels",
       description:
         "After you do your changes on OSM , Comeback to fAIr and fetch new labels, Give it a few min to update our database ( Click on button next to OSM Logo on AOI to fetch -Step 9)",
-      image: "/learn/12.png",
+      image: "/learn-resources/12.png",
     },
     {
       id: 14,
       title: "Step 14: Create Model for you Dataset",
       description:
         "Click on View Models on your dataset page , You will see this page. Click on Create New",
-      image: "/learn/13.png",
+      image: "/learn-resources/13.png",
     },
     {
       id: 15,
       title: "Step 15: Provide Model Metadata",
       description:
         "Give your model name , Choose Your Dataset from Dropdown , Select BaseModel : RAMP is default for now , Click on Create AI Model",
-      image: "/learn/14.png",
+      image: "/learn-resources/14.png",
     },
     {
       id: 16,
