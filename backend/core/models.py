50688ecee964ecfa9f067a794a2a594e707f0e4d
Antony Nyagah <tony.m.nyagah@gmail.com>
2024-03-13 12:30:28 +0300

    add tests for models in the core app

--- backend/core/models.py
+++ backend/core/models.py
@@ -39,6 +39,9 @@ class AOI(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.dataset.name} - {self.geom}"
+
 
 class Label(models.Model):
     aoi = models.ForeignKey(AOI, to_field="id", on_delete=models.CASCADE)
@@ -46,6 +49,9 @@ class Label(models.Model):
     osm_id = models.BigIntegerField(null=True, blank=True)
     tags = models.JSONField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.aoi} - {self.geom}"
 
 
 class Model(models.Model):
@@ -60,7 +66,10 @@ class Model(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(OsmUser, to_field="osm_id", on_delete=models.CASCADE)
     published_training = models.PositiveIntegerField(null=True, blank=True)
-    status = models.IntegerField(default=-1, choices=ModelStatus.choices)  #
+    status = models.IntegerField(default=-1, choices=ModelStatus.choices)
+
+    def __str__(self):
+        return self.name
 
 
 class Training(models.Model):
@@ -90,6 +99,9 @@ class Training(models.Model):
     batch_size = models.PositiveIntegerField()
     freeze_layers = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.model.name
+
 
 class Feedback(models.Model):
     FEEDBACK_TYPE = (
@@ -109,6 +121,9 @@ class Feedback(models.Model):
     user = models.ForeignKey(OsmUser, to_field="osm_id", on_delete=models.CASCADE)
     source_imagery = models.URLField()
 
+    def __str__(self):
+        return f"{self.user} - {self.training} - {self.feedback_type}"
+
 
 class FeedbackAOI(models.Model):
     class DownloadStatus(models.IntegerChoices):
@@ -124,6 +139,9 @@ class FeedbackAOI(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     source_imagery = models.URLField()
     user = models.ForeignKey(OsmUser, to_field="osm_id", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} - {self.training} - {self.source_imagery}"
 
 
 class FeedbackLabel(models.Model):
--- backend/tests/test_models_core.py
+++ backend/tests/test_models_core.py
@@ -1,4 +1,13 @@
-from core.models import AOI, Dataset
+from core.models import (
+    AOI,
+    Dataset,
+    Feedback,
+    FeedbackAOI,
+    FeedbackLabel,
+    Label,
+    Model,
+    Training,
+)
 from django.contrib.gis.geos import Polygon
 from django.test import TestCase
 from login.models import OsmUser
@@ -13,13 +22,15 @@ class DatasetModelTest(TestCase):
             name="Test Dataset",
             created_by=cls.osm_user,
             source_imagery="http://example.com/image.png",
-            status=Dataset.DatasetStatus.ACTIVE,
         )
 
     def test_dataset_creation(self):
         # Test the Dataset instance has been created properly.
         self.assertTrue(isinstance(self.dataset, Dataset))
-        self.assertEqual(self.dataset.__str__(), self.dataset.name)
+
+    def test_string_representation(self):
+        # Test the string representation of a Dataset instance
+        self.assertEqual(str(self.dataset), "Test Dataset")
 
     def test_invalid_dataset_creation(self):
         # Raise an exception if an invalid dataset is created
@@ -31,24 +42,19 @@ class DatasetModelTest(TestCase):
                 status=Dataset.DatasetStatus.ACTIVE,
             )
 
-    def test_dataset_status(self):
-        # Test the status field works as expected
-        self.assertEqual(self.dataset.status, Dataset.DatasetStatus.ACTIVE)
+    def test_default_dataset_status(self):
+        # Test the default dataset status is DRAFT
+        self.assertEqual(self.dataset.status, Dataset.DatasetStatus.DRAFT)
 
     def test_dataset_fields(self):
         # Test all fields for correct values
         self.assertEqual(self.dataset.name, "Test Dataset")
         self.assertEqual(self.dataset.created_by, self.osm_user)
         self.assertEqual(self.dataset.source_imagery, "http://example.com/image.png")
-        self.assertEqual(self.dataset.status, Dataset.DatasetStatus.ACTIVE)
         # Ensure auto_now_add fields are populated
         self.assertIsNotNone(self.dataset.created_at)
         # Ensure auto_now fields are populated
         self.assertIsNotNone(self.dataset.last_modified)
-
-    def test_string_representation(self):
-        # Test the string representation of a Dataset instance
-        self.assertEqual(str(self.dataset), "Test Dataset")
 
 
 class AOIModelTest(TestCase):
@@ -61,7 +67,6 @@ class AOIModelTest(TestCase):
                 name="Test Dataset",
                 created_by=cls.osm_user,
                 source_imagery="http://example.com/image.png",
-                status=Dataset.DatasetStatus.ACTIVE,
             ),
             geom=Polygon(
                 ((0, 0), (0, 1), (1, 1), (1, 0), (0, 0)),
@@ -69,11 +74,15 @@ class AOIModelTest(TestCase):
             ),
         )
 
-    def test_aoi_creation(self):
+    def test_AOI_creation(self):
         # Test the AOI instance has been created properly.
         self.assertTrue(isinstance(self.aoi, AOI))
+
+    def test_string_representation(self):
+        # Test the string representation of an AOI instance
         self.assertEqual(
-            self.aoi.__str__(), f"{self.aoi.dataset.name} - {self.aoi.geom}"
+            str(self.aoi),
+            f"Test Dataset - {self.aoi.geom}",
         )
 
     def test_invalid_AOI_creation(self):
@@ -91,3 +100,278 @@ class AOIModelTest(TestCase):
     def test_default_label_status(self):
         # The default label status should be NOT_DOWNLOADED
         self.assertEqual(self.aoi.label_status, AOI.DownloadStatus.NOT_DOWNLOADED)
+
+
+class LabelModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        cls.osm_user = OsmUser.objects.create(osm_id="123456789", username="testuser")
+        cls.aoi = AOI.objects.create(
+            dataset=Dataset.objects.create(
+                name="Test Dataset",
+                created_by=cls.osm_user,
+                source_imagery="http://example.com/image.png",
+                status=Dataset.DatasetStatus.ACTIVE,
+            ),
+            geom=Polygon(
+                ((0, 0), (0, 1), (1, 1), (1, 0), (0, 0)),
+                ((0.4, 0.4), (0.4, 0.6), (0.6, 0.6), (0.6, 0.4), (0.4, 0.4)),
+            ),
+        )
+        cls.label = Label.objects.create(
+            aoi=cls.aoi,
+            geom=Polygon(
+                ((0, 0), (0, 1), (1, 1), (1, 0), (0, 0)),
+                ((0.4, 0.4), (0.4, 0.6), (0.6, 0.6), (0.6, 0.4), (0.4, 0.4)),
+            ),
+            osm_id=123456789,
+            tags={"key": "value"},
+        )
+
+    def test_label_creation(self):
+        # Test the Label instance has been created properly.
+        self.assertTrue(isinstance(self.label, Label))
+
+    def test_string_representation(self):
+        # Test the string representation of an Label instance
+        self.assertEqual(
+            str(self.label),
+            f"{self.label.aoi} - {self.label.geom}",
+        )
+
+    def test_invalid_label_creation(self):
+        # Raise an exception if an invalid label is created
+        with self.assertRaises(Exception):
+            self.label = Label.objects.create(
+                aoi=None,
+                geom=Polygon(
+                    ((0, 0), (0, 1), (1, 1), (1, 0), (0, 0)),
+                    ((0.4, 0.4), (0.4, 0.6), (0.6, 0.6), (0.6, 0.4), (0.4, 0.4)),
+                ),
+                osm_id=123456789,
+                tags={"key": "value"},
+            )
+
+    def test_created_at_field(self):
+        # Test the created_by field is not empty
+        self.assertIsNotNone(self.label.created_at)
+
+
+class ModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        cls.osm_user = OsmUser.objects.create(osm_id="123456789", username="testuser")
+        cls.dataset = Dataset.objects.create(
+            name="Test Dataset",
+            created_by=cls.osm_user,
+            source_imagery="http://example.com/image.png",
+        )
+        cls.model = Model.objects.create(
+            name="Test Model",
+            created_by=cls.osm_user,
+            dataset=cls.dataset,
+        )
+
+    def test_model_creation(self):
+        # Test the Model instance has been created properly.
+        self.assertTrue(isinstance(self.model, Model))
+
+    def test_invalid_model_creation(self):
+        # Raise an exception if an invalid model is created
+        with self.assertRaises(Exception):
+            self.model = Model.objects.create(
+                name="Test Model",
+                created_by=None,
+                dataset=self.dataset,
+            )
+
+    def test_string_representation(self):
+        # Test the string representation of a Model instance
+        self.assertEqual(str(self.model), self.model.name)
+
+    def test_created_by_field(self):
+        # Test the created_by field is not empty
+        self.assertIsNotNone(self.model.created_by)
+
+
+class TrainingModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        cls.osm_user = OsmUser.objects.create(osm_id="123456789", username="testuser")
+        cls.dataset = Dataset.objects.create(
+            name="Test Dataset",
+            created_by=cls.osm_user,
+        )
+        cls.model = Model.objects.create(
+            name="Test Model",
+            created_by=cls.osm_user,
+            dataset=cls.dataset,
+        )
+        cls.training = Training.objects.create(
+            model=cls.model,
+            zoom_level=[19, 20, 21, 22],
+            created_by=cls.osm_user,
+            epochs=10,
+            batch_size=32,
+        )
+        cls.source_imagery = "http://example.com/image.png"
+        cls.description = "Test description"
+
+    def test_training_creation(self):
+        # Test the training instance has been created properly.
+        self.assertTrue(isinstance(self.training, Training))
+        self.assertIsNotNone(self.training)
+
+    def test_string_representation(self):
+        # Test the string representation of an AOI instance
+        self.assertEqual(str(self.training), (self.model.name))
+
+    def test_default_training_status(self):
+        # Test the default status is "SUBMITTED"
+        self.assertEqual(self.training.status, "SUBMITTED")
+
+
+class FeedbackModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        cls.osm_user = OsmUser.objects.create(osm_id="123456789", username="testuser")
+        cls.model = Model.objects.create(
+            name="Test Model",
+            created_by=cls.osm_user,
+            dataset=Dataset.objects.create(
+                name="Test Dataset",
+                created_by=cls.osm_user,
+            ),
+        )
+        cls.feedback = Feedback.objects.create(
+            user=cls.osm_user,
+            training=Training.objects.create(
+                model=cls.model,
+                zoom_level=[19, 20, 21, 22],
+                created_by=cls.osm_user,
+                epochs=10,
+                batch_size=32,
+            ),
+            geom=Polygon(
+                ((0, 0), (0, 1), (1, 1), (1, 0), (0, 0)),
+                ((0.4, 0.4), (0.4, 0.6), (0.6, 0.6), (0.6, 0.4), (0.4, 0.4)),
+            ),
+            zoom_level=19,
+            source_imagery="http://example.com/image.png",
+        )
+
+    def test_feedback_creation(self):
+        # Test the feedback instance has been created properly.
+        self.assertTrue(isinstance(self.feedback, Feedback))
+        self.assertIsNotNone(self.feedback)
+
+    def test_string_representation(self):
+        # Test the string representation of a Feedback instance
+        self.assertEqual(
+            str(self.feedback),
+            f"{self.feedback.user} - {self.feedback.training} - {self.feedback.feedback_type}",
+        )
+
+    def test_created_at_field(self):
+        # Test the created_at field is not empty
+        self.assertIsNotNone(self.feedback.created_at)
+
+
+class FeedbackAOITest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        cls.osm_user = OsmUser.objects.create(osm_id="123456789", username="testuser")
+        cls.dataset = Dataset.objects.create(
+            name="Test Dataset",
+            created_by=cls.osm_user,
+        )
+        cls.model = Model.objects.create(
+            name="Test Model",
+            created_by=cls.osm_user,
+            dataset=cls.dataset,
+        )
+        cls.training = Training.objects.create(
+            model=cls.model,
+            zoom_level=[19, 20, 21, 22],
+            created_by=cls.osm_user,
+            epochs=10,
+            batch_size=32,
+        )
+        cls.feedbackAOI = FeedbackAOI.objects.create(
+            user=cls.osm_user,
+            training=cls.training,
+            source_imagery="http://example.com/aoi_image.png",
+            geom=Polygon(
+                ((0, 0), (0, 1), (1, 1), (1, 0), (0, 0)),
+                ((0.4, 0.4), (0.4, 0.6), (0.6, 0.6), (0.6, 0.4), (0.4, 0.4)),
+            ),
+        )
+
+    def test_feedback_aoi_creation(self):
+        # Test the feedback aoi instance has been created properly.
+        self.assertTrue(isinstance(self.feedbackAOI, FeedbackAOI))
+        self.assertIsNotNone(self.feedbackAOI)
+
+    def test_string_representation(self):
+        # Test the string representation of a FeedbackAOI instance
+        self.assertEqual(
+            str(self.feedbackAOI),
+            f"{self.feedbackAOI.user} - {self.feedbackAOI.training} - {self.feedbackAOI.source_imagery}",
+        )
+
+
+class FeedbackLabelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        cls.osm_user = OsmUser.objects.create(
+            osm_id="987654321", username="feedbacklabeluser"
+        )
+        cls.dataset = Dataset.objects.create(
+            name="Feedback Label Dataset",
+            created_by=cls.osm_user,
+        )
+        cls.model = Model.objects.create(
+            name="Feedback Label Model",
+            created_by=cls.osm_user,
+            dataset=cls.dataset,
+        )
+        cls.training = Training.objects.create(
+            model=cls.model,
+            zoom_level=[19, 20, 21, 22],
+            created_by=cls.osm_user,
+            epochs=5,
+            batch_size=16,
+        )
+        cls.feedbackAOI = FeedbackAOI.objects.create(
+            user=cls.osm_user,
+            training=cls.training,
+            source_imagery="http://example.com/feedback_aoi_image.png",
+            geom=Polygon(
+                ((0, 0), (0, 2), (2, 2), (2, 0), (0, 0)),
+                ((0.5, 0.5), (0.5, 1.5), (1.5, 1.5), (1.5, 0.5), (0.5, 0.5)),
+            ),
+        )
+        cls.feedbackLabel = FeedbackLabel.objects.create(
+            osm_id=123456789,
+            feedback_aoi=cls.feedbackAOI,
+            tags={"natural": "tree"},
+            geom=Polygon(((0.5, 0.5), (0.5, 1.5), (1.5, 1.5), (1.5, 0.5), (0.5, 0.5))),
+        )
+
+    def test_feedback_label_creation(self):
+        # Test the feedback label instance has been created properly.
+        self.assertTrue(isinstance(self.feedbackLabel, FeedbackLabel))
+        self.assertIsNotNone(self.feedbackLabel)
+
+    def test_feedback_label_fields(self):
+        # Test the fields of the feedback label instance
+        self.assertEqual(self.feedbackLabel.osm_id, 123456789)
+        self.assertEqual(self.feedbackLabel.feedback_aoi, self.feedbackAOI)
+        self.assertEqual(self.feedbackLabel.tags, {"natural": "tree"})
+        self.assertTrue(isinstance(self.feedbackLabel.geom, Polygon))
