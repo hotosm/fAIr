9a5e95141831f42a9cd7463ccef75743f6651d92
jeafreezy <jolaiyaemmanuel@gmail.com>
2024-09-10 19:47:17 +0200

    feat: setup vite + react + ts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -18,7 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)} >
           count is {count}
         </button>
         <p>
