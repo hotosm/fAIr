828b629dd1ea67aaa770b62a2c96de2c6e6a72fc
jeafreezy <jolaiyaemmanuel@gmail.com>
2024-09-18 03:32:23 +0200

    landing page progress

---
+++ frontend/src/components/ui/header/navbar.tsx
@@ -0,0 +1,45 @@
+import Button from '../button/button'
+import './styles.css'
+import HOTFairLogo from '@/assets/hot_fair_logo.png'
+
+type NavBarLinks = {
+  title: string
+  href: string
+}[]
+
+const navLinks: NavBarLinks = [
+  {
+    title: 'Explore Models',
+    href: '#'
+  },
+  {
+    title: 'Learn',
+    href: '#'
+  },
+  {
+    title: 'About',
+    href: '#'
+  },
+  {
+    title: 'Resources',
+    href: '#'
+  }
+]
+
+const NavBar = () => {
+  return (
+    <nav>
+      <img src={HOTFairLogo} alt='HOT fAIr Logo' title='HOT fAIr Logo'></img>
+      <ul>
+        {navLinks.map(link => (
+          <li>
+            <a href={link.href}>{link.title}</a>
+          </li>
+        ))}
+      </ul>
+      <Button variant='primary'>Login</Button>
+    </nav>
+  )
+}
+
+export default NavBar
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './styles/hot-sl.css'
+import './styles/index.css'
+import { App } from './app'
+
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path'
+
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.16.0/cdn/')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
