f7f10d1d07f524b4689acbe41e6a91ddf58ccc0e
jeafreezy <jolaiyaemmanuel@gmail.com>
2024-09-26 19:35:47 +0200

    chore: testing husky and lint staged

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,7 +7,7 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, "prettier"],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
