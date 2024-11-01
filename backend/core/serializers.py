8067e163616ac2cf03682d8f9dab794637fa985f
kshitijrajsharma <skshitizraj@gmail.com>
2024-11-01 12:42:24 +0100

    Add user information on the AOI serializer for consistency

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -142,6 +142,18 @@ class AOISerializer(
             "label_status",
         )
 
+    def create(self, validated_data):
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            validated_data["user"] = request.user
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            validated_data["user"] = request.user
+        return super().update(instance, validated_data)
+
 
 class FeedbackAOISerializer(GeoFeatureModelSerializer):
     class Meta:
