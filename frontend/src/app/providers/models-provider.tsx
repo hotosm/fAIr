fbe39f979f5b12282894779c48df6de757fbf317
jeafreezy <jolaiyaemmanuel@gmail.com>
2024-12-06 13:38:35 +0100

    chore: fixed bug in model details

--- frontend/src/app/providers/models-provider.tsx
+++ frontend/src/app/providers/models-provider.tsx
@@ -306,8 +306,8 @@ export const ModelsProvider: React.FC<{
     }
 
     handleChange(MODEL_CREATION_FORM_NAME.BASE_MODELS, data.base_model);
-    handleChange(MODEL_CREATION_FORM_NAME.MODEL_DESCRIPTION, data.description);
-    handleChange(MODEL_CREATION_FORM_NAME.MODEL_NAME, data.name);
+    handleChange(MODEL_CREATION_FORM_NAME.MODEL_DESCRIPTION, data.description ?? '');
+    handleChange(MODEL_CREATION_FORM_NAME.MODEL_NAME, data.name ?? '');
     handleChange(
       MODEL_CREATION_FORM_NAME.SELECTED_TRAINING_DATASET_ID,
       data.dataset,
@@ -318,10 +318,10 @@ export const ModelsProvider: React.FC<{
   useEffect(() => {
     if (!isEditMode || trainingDatasetIsPending || trainingDatasetIsError)
       return;
-    handleChange(MODEL_CREATION_FORM_NAME.DATASET_NAME, trainingDataset.name);
+    handleChange(MODEL_CREATION_FORM_NAME.DATASET_NAME, trainingDataset.name ?? '');
     handleChange(
       MODEL_CREATION_FORM_NAME.TMS_URL,
-      trainingDataset.source_imagery,
+      trainingDataset.source_imagery ?? '',
     );
   }, [
     isEditMode,
