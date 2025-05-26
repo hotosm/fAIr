5cc850ed7b7172296f29df0568f1085b37f601f3
kshitijrajsharma <skshitizraj@gmail.com>
2025-05-26 12:01:30 +0200

    fix: comment out unused feedback-related URL patterns in urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -44,8 +44,8 @@ router.register(r"label", LabelViewSet)
 router.register(r"training", TrainingViewSet)
 router.register(r"model", ModelViewSet)
 router.register(r"feedback", FeedbackViewset)
-router.register(r"feedback-aoi", FeedbackAOIViewset)
-router.register(r"feedback-label", FeedbackLabelViewset)
+# router.register(r"feedback-aoi", FeedbackAOIViewset)
+# router.register(r"feedback-label", FeedbackLabelViewset)
 router.register(r"banner", BannerViewSet)
 router.register(r"notifications/me", UserNotificationViewSet, basename="notifications")
 router.register(r"prediction", PredictionViewSet)
@@ -55,10 +55,10 @@ urlpatterns = [
     path("", include(router.urls)),
     path("label/osm/fetch/<int:aoi_id>/", RawdataApiAOIView.as_view()),
     path("label/upload/<int:aoi_id>/", LabelUploadView.as_view(), name="label-upload"),
-    path(
-        "label/feedback/osm/fetch/<int:feedbackaoi_id>/",
-        RawdataApiFeedbackView.as_view(),
-    ),
+    # path(
+    #     "label/feedback/osm/fetch/<int:feedbackaoi_id>/",
+    #     RawdataApiFeedbackView.as_view(),
+    # ),
     path("users/", UsersView.as_view(), name="user-list-view"),
     path("models/centroid/", ModelCentroidView.as_view(), name="models-centroid"),
     path("datasets/centroid/", DatasetCentroidView.as_view(), name="datasets-centroid"),
@@ -70,14 +70,14 @@ urlpatterns = [
         TerminateTrainingView.as_view(),
         name="cancel_training",
     ),
-    path("feedback/training/submit/", FeedbackView.as_view()),
+    # path("feedback/training/submit/", FeedbackView.as_view()),
     # path("status/", APIStatus.as_view()),
     # path("geojson2osm/", geojson2osmconverter, name="geojson2osmconverter"),
-    path("conflate/", ConflateGeojson, name="Conflate Geojson"),
-    path("aoi/gpx/<int:aoi_id>/", GenerateGpxView.as_view()),
-    path(
-        "feedback-aoi/gpx/<int:feedback_aoi_id>/", GenerateFeedbackAOIGpxView.as_view()
-    ),
+    # path("conflate/", ConflateGeojson, name="Conflate Geojson"),
+    # path("aoi/gpx/<int:aoi_id>/", GenerateGpxView.as_view()),
+    # path(
+    #     "feedback-aoi/gpx/<int:feedback_aoi_id>/", GenerateFeedbackAOIGpxView.as_view()
+    # ),
     path(
         "workspace/download/<path:lookup_dir>/", TrainingWorkspaceDownloadView.as_view()
     ),
