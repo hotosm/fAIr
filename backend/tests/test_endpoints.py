b4c867403e6211aa63130bc5a65c6622fd5663dc
kshitijrajsharma <skshitizraj@gmail.com>
2025-06-03 09:36:38 +0200

    fix: update endpoint path for training status in test_endpoints.py

--- backend/tests/test_endpoints.py
+++ backend/tests/test_endpoints.py
@@ -255,7 +255,7 @@ class TaskApiTest(APILiveServerTestCase):
         # get running training status
 
         res = self.client.get(
-            f"{API_BASE}/training/status/{training.id}/", headers=headersList
+            f"{API_BASE}/task/status/{training.id}/", headers=headersList
         )
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
