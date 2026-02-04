d3ecc2ea6929853fec2185e67b8af353e77e096e
Emmanuel Jolaiya <jolaiyaemmanuel@gmail.com>
2026-02-04 13:53:55 +0100

    chore: put slug in url when learn video opens

--- frontend/src/app/routes/learn.tsx
+++ frontend/src/app/routes/learn.tsx
@@ -23,6 +23,10 @@ export const LearnPage = () => {
   
   const handleVideoSelect = (video: TFairVideo) => {
     setSelectedVideo(video);
+    navigate(`${APPLICATION_ROUTES.LEARN_BASE}/${video.slug}`, {
+      replace: false,
+      preventScrollReset: true,
+    });
   };
 
   useEffect(() => {
