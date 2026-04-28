c5303cb757e074dd006ca4efb49081e8d33125e7
hg1g <hernan@lawal.coop>
2026-04-28 18:08:38 -0300

    Drop defensive try/except around PAT init

--- backend/fairproject/settings.py
+++ backend/fairproject/settings.py
@@ -168,19 +168,16 @@ if AUTH_PROVIDER == AuthProvider.HANKO:
     LOGIN_INTERNAL_API_KEY = env("LOGIN_INTERNAL_API_KEY", default="")
     LOGIN_BACKEND_URL = env("LOGIN_BACKEND_URL", default=LOGIN_URL)
     if LOGIN_INTERNAL_API_KEY:
-        try:
-            from hotosm_auth import remote_pat_resolver
-            from hotosm_auth_django import init_auth_django
+        from hotosm_auth import remote_pat_resolver
+        from hotosm_auth_django import init_auth_django
 
-            init_auth_django(
-                app_name="fair",
-                pat_resolver=remote_pat_resolver(
-                    login_url=LOGIN_BACKEND_URL,
-                    internal_key=LOGIN_INTERNAL_API_KEY,
-                ),
-            )
-        except ImportError:
-            pass  # auth-libs version without PAT support
+        init_auth_django(
+            app_name="fair",
+            pat_resolver=remote_pat_resolver(
+                login_url=LOGIN_BACKEND_URL,
+                internal_key=LOGIN_INTERNAL_API_KEY,
+            ),
+        )
 
 ROOT_URLCONF = "fairproject.urls"
 WSGI_APPLICATION = "fairproject.wsgi.application"
