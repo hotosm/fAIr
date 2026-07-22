41513b4582094d2363413112084c7c63141c8206
shittu adewale <sheriffhardewale@gmail.com>
2026-07-22 11:32:56 +0100

    feat: updated icons for map large area

--- frontend/src/features/try-fair/components/start-mapping/map-large-area-modal.tsx
+++ frontend/src/features/try-fair/components/start-mapping/map-large-area-modal.tsx
@@ -1,33 +1,45 @@
 import { MapComponent } from "@/components/map";
 import { Dialog } from "@/components/ui/dialog";
-import { MapIcon, LayerStackIcon, UploadIcon } from "@/components/ui/icons";
+import { MapIcon, UploadIcon } from "@/components/ui/icons";
+import { DrawIcon } from "@/components/ui/icons/draw-icon";
+import { PictureIcon } from "@/components/ui/icons/picture-icon";
 import { SHOELACE_SIZES } from "@/enums";
 import { useModalMap } from "@/features/try-fair/hooks/use-modal-map";
 import { BBOX, Feature } from "@/types";
+import { useState } from "react";
 
 // ── Tabs ────────────────────────────────────────────────────────────────────────
 
 type AOITab = "whole" | "draw" | "upload";
 
 const TABS: { value: AOITab; label: string; Icon: typeof MapIcon }[] = [
-  { value: "whole", label: "Whole Imagery", Icon: MapIcon },
-  { value: "draw", label: "Draw Specific Area", Icon: LayerStackIcon },
+  { value: "whole", label: "Whole Imagery", Icon: PictureIcon },
+  { value: "draw", label: "Draw Specific Area", Icon: DrawIcon },
   { value: "upload", label: "Upload Area of Interest", Icon: UploadIcon },
 ];
 
 const MapLargeAreaContent = ({ tileServerURL }: { tileServerURL?: string }) => {
   const { mapContainerRef, map } = useModalMap();
+  const [activeTab, setActiveTab] = useState<string>("draw")
 
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex border-gray-border gap-4 justify-between w-full border p-2 rounded-lg">
-        {TABS.map((tab) => (
-          <div
-            className="p-3 bg-off-white rounded-lg flex items-center justify-center w-full"
-            key={tab.value}
+      <div className="flex border-gray-border gap-4 justify-between w-full border p-2 rounded-md">
+        {TABS.map(({ value, label, Icon }) => (
+          <button
+          onClick={() => setActiveTab(value)}
+            className={
+              activeTab === value ?
+              "p-3 gap-2 bg-secondary rounded-lg flex items-center justify-center w-full"
+              :
+
+              "p-3 gap-2 bg-off-white rounded-lg flex items-center justify-center w-full"
+            }
+            key={value}
           >
-            <p className="text-sm">{tab.label}</p>
-          </div>
+            <Icon className="size-5" />
+            <p className="text-sm">{label}</p>
+          </button>
         ))}
       </div>
       <div className="relative h-[520px] rounded-[18px] overflow-hidden w-full z-10">
@@ -35,7 +47,7 @@ const MapLargeAreaContent = ({ tileServerURL }: { tileServerURL?: string }) => {
           map={map}
           mapContainerRef={mapContainerRef}
           tileServiceURL={tileServerURL}
-          zoomControls={false}
+          zoomControls={true}
         />
       </div>
     </div>
